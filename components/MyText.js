e6a1e13b076c7c2e05f53ed38c0b999f3ae7194d
nimrodsilberfeld <nimrod513@gmail.com>
2021-11-04 12:25:42 +0200

    init

--- components/MyText.js
+++ components/MyText.js
@@ -4,7 +4,5 @@ import colors from "../constans/colors"
 import { ThemeContext } from "../utils/ThemeProvider"
 export const MyText = (props) => {
     const { darkTheme } = useContext(ThemeContext)
-
-    //return <Text {...props} style={[{ fontFamily: "Ubuntu_400Regular", color: colors('text', darkTheme) }, props.style]}>{props.children}</Text>
     return <Text {...props} style={[{ color: colors('text', darkTheme) }, props.style]}>{props.children}</Text>
 }
